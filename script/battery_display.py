634515285b75c7d3aaa479b831ff9c90cbcff84f
hamyyy <oudy_1999@hotmail.com>
2023-07-10 19:56:57 +1000

    gui

--- script/battery_display.py
+++ script/battery_display.py
@@ -3,7 +3,7 @@
 import PySimpleGUI as sg
 import rospy
 
-from pr2_msgs.msg import BatteryServer
+from pr2_msgs.msg import BatteryServer2
 
 charge = [0] * 4
 
@@ -14,13 +14,19 @@ layout = [  [sg.Text('Some text on Row 1')],
 
 window = sg.Window('Window Title', layout, no_titlebar=True)
 
-def on_battery_msg(msg: BatteryServer):
-  charge[msg.id - 1] = msg.averageCharge
+def on_battery_msg(msg: BatteryServer2):
+  charge[msg.id - 1] = msg.average_charge
   rospy.loginfo(f"{charge} : {msg.id}")
   layout[0] = [sg.Text(f'Some text on Row 1 ({charge[0]})')]
+  for bat in msg.battery:
+    bat.battery_register = []
+    bat.battery_update_flag = []
+    bat.battery_register_update = []
+
+  rospy.loginfo(msg)
 
 rospy.init_node("pi_battery_display_node")
-sub = rospy.Subscriber("/battery/server", BatteryServer, on_battery_msg)
+sub = rospy.Subscriber("/battery/server2", BatteryServer2, on_battery_msg)
 
 while not rospy.is_shutdown():
     event, values = window.read(timeout=10)
