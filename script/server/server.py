188563d950377229f881d7c18a20c9e74ace1381
hamyyy <oudy_1999@hotmail.com>
2023-07-11 16:04:49 +1000

    fixed browser

--- script/server/server.py
+++ script/server/server.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 from flask import Flask, send_from_directory
 import webbrowser
 
@@ -17,8 +18,10 @@ def home(path):
 
 
 def main():
-    webbrowser.open_new('http://127.0.0.1:5000/')
+    os.system("midori -e Fullscreen http://127.0.0.1:5000/ &")
+    #webbrowser.open_new('http://127.0.0.1:5000/')
     app.run(host="127.0.0.1", port=5000)
+    os.system("pkill midori")
 
 
 if __name__ == '__main__':
